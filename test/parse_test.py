65d61d37f4e51c3de670f59ea45afba3f112c985
khazhyk <khazhy@gmail.com>
2019-10-12 16:02:48 -0700

    add OverwatchProfile.private attribute, and don't try to parse private profiles

--- test/parse_test.py
+++ test/parse_test.py
@@ -12,3 +12,12 @@ class ParseTest(unittest.TestCase):
         with requests.get(bt.profile_url) as resp:
             prof = pachimari.OverwatchProfile.from_html(
                 bt, resp.text)
+
+    def test_private_profile(self):
+        bt = pachimari.BattleTag('pc', 'us', 'khazhyk', '1819')
+
+        with open("test/test_data/sample_private.html", "r", encoding="utf8") as f:
+            prof = pachimari.OverwatchProfile.from_html(bt, f.read())
+
+        self.assertTrue(prof.private)
+        self.assertEqual(prof.level, 33)
